8feec7f89e6d954f130a86786db326ab6d1da509
Florian Straub <Florian.Straub@dm.de>
2018-05-12 16:54:03 +0200

    Replacing the Conditional Logic on Price Code with Polymorphism and adapting tests.

--- test/MovieTest.java
+++ test/MovieTest.java
@@ -10,7 +10,7 @@ public class MovieTest {
     private Movie movie;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         movie = new Movie(MOVIE_TITLE, Movie.REGULAR);
     }
 
@@ -29,5 +29,41 @@ public class MovieTest {
     public void getTitle() {
         assertEquals(movie.getTitle(), MOVIE_TITLE);
     }
+
+    @Test
+    public void chargeAmountRegular() {
+        movie.setPriceCode(Movie.REGULAR);
+        double actual = movie.getCharge(RentalTest.DAYS_RENTED);
+        assertEquals(5d, actual, 0);
+    }
+
+    @Test
+    public void chargeAmountChildren() {
+        movie.setPriceCode(Movie.CHILDRENS);
+        double actual = movie.getCharge(RentalTest.DAYS_RENTED);
+        assertEquals(3d, actual, 0);
+    }
+
+    @Test
+    public void chargeAmountNewRelease() {
+        movie.setPriceCode(Movie.NEW_RELEASE);
+        double actual = movie.getCharge(RentalTest.DAYS_RENTED);
+        assertEquals(12d, actual, 0);
+    }
+
+    @Test
+    public void getFrequentRenterPointsOne() {
+        movie.setPriceCode(Movie.NEW_RELEASE);
+        int actual = movie.getFrequentRenterPoints(RentalTest.DAYS_RENTED_ONE);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void getFrequentRenterPointsTwo() {
+        movie.setPriceCode(Movie.NEW_RELEASE);
+        int actual = movie.getFrequentRenterPoints(RentalTest.DAYS_RENTED);
+        assertEquals(2, actual);
+    }
+
 
 }
--- test/RentalTest.java
+++ test/RentalTest.java
@@ -27,41 +27,4 @@ public class RentalTest {
         assertEquals(rental.getDaysRented(), DAYS_RENTED);
     }
 
-    @Test
-    public void chargeAmountRegular(){
-        movie.setPriceCode(Movie.REGULAR);
-        double actual = rental.getCharge();
-        assertEquals(5d, actual, 0);
-    }
-
-    @Test
-    public void chargeAmountChildren(){
-        movie.setPriceCode(Movie.CHILDRENS);
-        double actual = rental.getCharge();
-        assertEquals(3d, actual, 0);
-    }
-
-    @Test
-    public void chargeAmountNewRelease(){
-        movie.setPriceCode(Movie.NEW_RELEASE);
-        double actual = rental.getCharge();
-        assertEquals(12d, actual, 0);
-    }
-
-    @Test
-    public void getFrequentRenterPointsOne(){
-        movie.setPriceCode(Movie.NEW_RELEASE);
-        rental = new Rental(movie, DAYS_RENTED_ONE);
-        int actual = rental.getFrequentRenterPoints();
-        assertEquals(1, actual);
-    }
-
-    @Test
-    public void getFrequentRenterPointsTwo(){
-        movie.setPriceCode(Movie.NEW_RELEASE);
-        rental = new Rental(movie, DAYS_RENTED);
-        int actual = rental.getFrequentRenterPoints();
-        assertEquals(2, actual);
-    }
-
 }
